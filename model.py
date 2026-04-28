c32b23a1d6c673dfdd4bdf5e4e843453ec6a63e6
ajerez479 <ajerez479@unab.edu.co>
2026-04-27 19:22:44 -0500

    Update model.py

--- model.py
+++ model.py
@@ -9,20 +9,24 @@ def preprocess_data(df, selected_features):
     X = df[selected_features].copy()
 
     # Keep only numeric columns
-    X = X.select_dtypes(include=["int64", "float64"])
+    numerical = X.select_dtypes(include=["int64", "float64"])
+    categorical = X.select_dtypes(include=["object"])
+   
 
     scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
+    X_scaled = scaler.fit_transform(X[numerical])
+    X_encoded = pd.get_dummies(X[categorical])
+    X = pd.concat([X_scaled, X_encoded], axis=1)
 
-    return X, X_scaled, scaler
+    return X
 
 
-def train_hierarchical(X_scaled, n_clusters):
+def train_hierarchical(X, n_clusters,linkage='ward'):
     """
     Train Agglomerative Clustering
     """
-    model = AgglomerativeClustering(n_clusters=n_clusters)
-    labels = model.fit_predict(X_scaled)
+    model = AgglomerativeClustering(n_clusters=n_clusters,linkage=linkage)
+    labels = model.fit_predict(X)
 
     return model, labels
 
@@ -31,17 +35,23 @@ def cluster_new_point(df, selected_features, new_point, n_clusters):
     """
     Refit model including new point (correct way for hierarchical)
     """
-    X = df[selected_features].copy()
-    X = X.select_dtypes(include=["int64", "float64"])
 
+    X = df[selected_features].copy()
     # Add new point
     new_df = pd.DataFrame([new_point], columns=selected_features)
-    X_extended = pd.concat([X, new_df], ignore_index=True)
+    X = pd.concat([X, new_df], ignore_index=True)
+
+    # Keep only numeric columns
+    numerical = X.select_dtypes(include=["int64", "float64"])
+    categorical = X.select_dtypes(include=["object"])
+   
 
     scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X_extended)
+    X_scaled = scaler.fit_transform(X[numerical])
+    X_encoded = pd.get_dummies(X[categorical])
+    X = pd.concat([X_scaled, X_encoded], axis=1)
 
     model = AgglomerativeClustering(n_clusters=n_clusters)
-    labels = model.fit_predict(X_scaled)
+    labels = model.fit_predict(X)
 
     return labels[-1]  # cluster of new point
