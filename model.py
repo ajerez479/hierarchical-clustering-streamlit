20850608abc2eb6b5a8a103602d1fce18bc2a429
ajerez479 <ajerez479@unab.edu.co>
2026-04-27 20:01:02 -0500

    Update model.py

--- model.py
+++ model.py
@@ -30,30 +30,3 @@ def hierarchical_model(X, n_clusters,linkage='ward'):
     labels = model.fit_predict(X)
 
     return model, labels
-
-
-def cluster_new_point(df, selected_features, new_point, n_clusters):
-    """
-    Refit model including new point (correct way for hierarchical)
-    """
-
-    X = df[selected_features].copy()
-    # Add new point
-    new_df = pd.DataFrame([new_point], columns=selected_features)
-    X = pd.concat([X, new_df], ignore_index=True)
-
-    # Keep only numeric columns
-    numerical = X.select_dtypes(include=["int64", "float64"]).columns
-    categorical = X.select_dtypes(include=["object"]).columns
-   
-
-    scaler = StandardScaler()
-    encoder = LabelEncoder()
-    X[numerical] = scaler.fit_transform(X[numerical])
-    X[categorical] = encoder.fit(X[categorical])
-    
-
-    model = AgglomerativeClustering(n_clusters=n_clusters)
-    labels = model.fit_predict(X)
-
-    return labels[-1]  # cluster of new point
